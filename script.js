34563bbfc426a63eaace61411fef77e9a0a4ec61
marck0101 <marcos.henrique.a7x@gmail.com>
2022-04-30 04:41:25 -0300

    envianndo arquivos

---
+++ script.js
@@ -0,0 +1,205 @@
+function home() {
+
+    exibe =
+
+        ` <div class=''>
+
+        <div class="flex-container">
+        <div class="cabecalho">
+            <div class="texto2">
+                <h1>home</h1>
+                <p>What is Lorem Ipsum?
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+                    and
+                    scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+                    leap
+                    into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with
+                    the
+                    release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+                    publishing
+                    software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+                <p> Why do we use it?
+                    It is a long established fact that a reader will be distracted by the readable content of a page
+                    when
+                    looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal
+                    distribution
+                    of letters, as opposed to using 'Content here, content here', making it look like readable English.
+                    Many
+                    desktop publishing packages and web page editors now use Lorem Ipsum as their default model text,
+                    and a
+                    search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have
+                    evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+                </p>
+            </div>
+        </div>
+    </div>
+
+        </div>`;
+
+    document.getElementById("grid").innerHTML = exibe;
+
+
+}
+
+
+function hobbie() {
+    exibe =
+
+        ` <div class=''>
+
+    <div class="flex-container">
+    <div class="cabecalho">
+        <div class="texto2">
+            <h1>Titulo</h1>
+            <p>What is Lorem Ipsum?
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+                and
+                scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+                leap
+                into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with
+                the
+                release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+                publishing
+                software like Aldus PageMaker including versions of Lorem Ipsum.
+                What is Lorem Ipsum?
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+                and
+                scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+                leap
+                into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with
+                the
+                release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+                publishing
+                software like Aldus PageMaker including versions of Lorem Ipsum.
+                </p>
+
+            <p> Why do we use it?
+                It is a long established fact that a reader will be distracted by the readable content of a page
+                when
+                looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal
+                distribution
+                of letters, as opposed to using 'Content here, content here', making it look like readable English.
+                Many
+                desktop publishing packages and web page editors now use Lorem Ipsum as their default model text,
+                and a
+                search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have
+                evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+                Why do we use it?
+                It is a long established fact that a reader will be distracted by the readable content of a page
+                when
+                looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal
+                distribution
+                of letters, as opposed to using 'Content here, content here', making it look like readable English.
+                Many
+                desktop publishing packages and web page editors now use Lorem Ipsum as their default model text,
+                and a
+                search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have
+                evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+            
+                </p>
+        </div>
+    </div>
+</div>
+
+    </div>`;
+
+    document.getElementById("grid").innerHTML = exibe;
+
+}
+
+function features() {
+    exibe =
+        ` <div class=''>
+    <div class="flex-container">
+    <div class="cabecalho">
+        <div class="texto2">
+            <h1>Titulo</h1>
+            <p>What is Lorem Ipsum?
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+                and
+                scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+                leap
+                into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with
+                the
+                release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+                publishing
+                software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+            <p> Why do we use it?
+                It is a long established fact that a reader will be distracted by the readable content of a page
+                when
+                looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal
+                distribution
+                of letters, as opposed to using 'Content here, content here', making it look like readable English.
+                Many
+                desktop publishing packages and web page editors now use Lorem Ipsum as their default model text,
+                and a
+                search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have
+                evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+            </p>
+        </div>
+    </div>
+</div>
+    </div>`;
+
+    document.getElementById("grid").innerHTML = exibe;
+
+}
+function contact() {
+    exibe =
+
+        ` <div class=''>
+
+        <div style="height: 300px;
+        display: flexbox;
+        flex-direction: column;
+        justify-content: space-between;
+    ">
+        <div style="
+            padding: auto;
+            background-color: pink;
+        
+            height: auto;
+            align-items: flex-end;
+            margin-top: auto;
+        ">
+            <div class="texto2">
+
+                <div style="   
+                align-items: center,
+                padding: 1em,
+                text-align: left,
+                justify-content: center,
+                text-align: justify
+            ">
+                    <h3>Assim que possível retornarei o contato</h3>
+                    <form action="https://formsubmit.co/tamires.muller@gmail.com" method="POST">
+                        <div>
+                            <label>Nome Completo</label>
+                            <input type="text" name="name" id="nomeCompleto" class="form-control "
+                                placeholder="Nome e Sobrenome" value="">
+                        </div> <br>
+
+                        <div>
+                            <label>E-MAIL</label>
+                            <input type="email" name="email" id="email" class="form-control "
+                                placeholder="nome@email.com" value="">
+                        </div><br>
+
+                        <button class="btn btn-secondary btn-lg btn-block" onclick="maeFunction() ">Ennviar</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+  
+        </div>`;
+
+    document.getElementById("grid").innerHTML = exibe;
+}
+
---
+++ teste1/script.js
@@ -0,0 +1,11 @@
+function testeBotao() {
+
+    console.log("puta que pariu");
+    alert("esse botaão está funcioonando");
+
+}
+
+function mae() {
+    console.log("entou na function mãe")
+    testeBotao();
+}
