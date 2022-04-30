1d77bb51f3da1bb7f7e6c534dd7224b16800662b
Marcos Henrique Corrêa <102032089+marck0101@users.noreply.github.com>
2022-04-30 04:46:12 -0300

    Update script.js
    ajuste

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ function hobbie() {
     <div class="flex-container">
     <div class="cabecalho">
         <div class="texto2">
-            <h1>Bobbie</h1>
+            <h1>Hobbie</h1>
             <p>What is Lorem Ipsum?
                 Lorem  Lorem Ipsum is simply Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
                 industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
